62dc296cccd4f846f435988b1327ae1b4629b5ce
Mihai <mihaighiga14@icloud.com>
2024-10-15 21:33:05 +0100

    test

---
+++ includes/head-tag-contents.php
@@ -0,0 +1,16 @@
+<title><?php print $PAGE_TITLE;?></title>
+
+<?php if ($CURRENT_PAGE == "Index") { ?>
+	<meta name="description" content="" />
+	<meta name="keywords" content="" /> 
+<?php } ?>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"><style>
+	#main-content {
+		margin-top:20px;
+	}
+	.footer {
+		font-size: 14px;
+		text-align: center;
+	}
+</style>
