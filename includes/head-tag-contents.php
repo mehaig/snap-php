1845b9e97f3192244382106335f77d91b2609b1d
Mihai <mihaighiga14@icloud.com>
2024-10-19 19:31:06 +0100

    "Added favicon link and Bootstrap Icons stylesheet to head tag contents"

--- includes/head-tag-contents.php
+++ includes/head-tag-contents.php
@@ -6,6 +6,7 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="<?php echo $baseUrl?>assets/images/box.svg">
     <title><?php print $PAGE_TITLE; ?></title>
 
     <?php if ($CURRENT_PAGE == "Index") { ?>
@@ -16,6 +17,7 @@
     <link rel="stylesheet" href="<?php echo $baseUrl; ?>assets/style.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
    
 
