e282847a1b078a3fe6df4e02e865f42877603a05
Mihai <mihaighiga14@icloud.com>
2024-10-19 17:39:10 +0100

    "Updated style.css and footer.php files"

--- components/footer.php
+++ components/footer.php
@@ -2,9 +2,9 @@
 
 
 <footer class="blog-footer ">
-	<p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+	<p>Theme build for snap-php</p>
 	<p>
-		<a href="#">Back to top</a>
+		<a href="https://github.com/mehaig/snap-php">Snap-PHP Repo</a>
 	</p>
 	&copy; <?php print date("Y"); ?>
 </footer>
